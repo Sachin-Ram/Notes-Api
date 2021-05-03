4d6676d49b30ee009f76a43f2b76678ae0d34b47
Sibidharan Nandhakumar <hello@sibidharan.me>
2021-05-03 16:35:51 +0530

    Updated connection settings

--- api/index.php
+++ api/index.php
@@ -6,15 +6,33 @@
 
         public $data = "";
 
-        const DB_SERVER = "localhost";
-        const DB_USER = "root";
-        const DB_PASSWORD = "";
-        const DB = "apis";
+        private $DB_SERVER = "localhost";
+        private $DB_USER = "root";
+        private $DB_PASSWORD = "";
+        private $DB_NAME = "apis";
 
         private $db = NULL;
 
         public function __construct(){
             parent::__construct();                // Init parent contructor
+            //read database config from ../../env.json
+            /*
+            file env.json
+
+            {
+                "database": "apis",
+                "username": "root",
+                "password": "",
+                "server": "localhost"
+            }
+
+            */
+            $config_json = file_get_contents('../../env.json');
+            $config = json_decode($config_json, true);
+            $this->DB_SERVER = $config['server'];
+            $this->DB_USER = $config['username'];
+            $this->DB_PASSWORD = $config['password'];
+            $this->DB_NAME = $config['database'];
             $this->dbConnect();                    // Initiate Database connection
         }
 
@@ -25,7 +43,7 @@
             if ($this->db != NULL) {
 				return $this->db;
 			} else {
-				$this->db = mysqli_connect(self::DB_SERVER,self::DB_USER,self::DB_PASSWORD, self::DB);
+				$this->db = mysqli_connect($this->DB_SERVER,$this->DB_USER,$this->DB_PASSWORD, $this->DB_NAME);
 				if (!$this->db) {
 					die("Connection failed: ".mysqli_connect_error());
 				} else {
