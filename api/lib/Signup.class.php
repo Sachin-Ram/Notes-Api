1239d8ff27637fd1299e85e7b58381a0c6861f9f
sachin <sachinram2510@gmail.com>
2024-04-04 20:17:35 +0530

    WIP:Sendgrid email fixed with PHP mailer

--- api/lib/Signup.class.php
+++ api/lib/Signup.class.php
@@ -1,104 +1,115 @@
 <?php
-
-require_once($_SERVER['DOCUMENT_ROOT'].'/api/lib/Database.class.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/api/lib/Folder.class.php');
-require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
-
-//TODO Homework: find why ../vendor? it is the same reason why we use ../../env.json in config.
-
-class Signup
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require_once('../vendor/autoload.php');
+class signup
 {
     private $username;
     private $password;
     private $email;
-
-    private $db;
+    private $id;
+    private $conn;
 
     public function __construct($username, $password, $email)
     {
-        $this->db = Database::getConnection();
-        $this->username = $username;
-        $this->password = $password;
-        $this->email = $email;
-        if ($this->userExists()) {
-            throw new Exception("User already exists");
+        $this->conn=database::getconnection();
+        $this->username=$username;
+        $this->password=$password;
+        $this->email=$email;
+        if($this->userexists($username)){
+             throw new Exception("user already exists");  
         }
+        else{
+        $pass=$this->hashpass();
         $bytes = random_bytes(16);
-        $this->token = $token = bin2hex($bytes); //to verify users over email.
-        $password = $this->hashPassword();
-        //Homework - make a proper flow to throw username already exists
-        $query = "INSERT INTO `auth` (`username`, `password`, `email`, `active`, `token`) VALUES ('$username', '$password', '$email', 0, '$token');";
-        if (!mysqli_query($this->db, $query)) {
-            throw new Exception("Unable to signup, user account might already exist.");
-        } else {
-            $this->id = mysqli_insert_id($this->db);
-            // $this->sendVerificationMail();
-            $f = new Folder();
-            session_start();
-            $_SESSION['username'] = $this->username;
-            $f->createNew('Default Folder');
-        }
-    }
+        $token=bin2hex($bytes);
 
-    public function sendVerificationMail()
-    {
-        // $config_json = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../env.json');
-        // $config = json_decode($config_json, true);
-        // $token = $this->token;
-        // $email = new \SendGrid\Mail\Mail();
-        // $email->setFrom("noreply@selfmade.ninja", "API Course by Selfmade");
-        // $email->setSubject("Verify your account");
-        // $email->addTo($this->email, $this->username);
-        // $email->addContent("text/plain", "Please verify your account at: https://api1.selfmade.ninja/verify?token=$token");
-        // $email->addContent(
-        //     "text/html",
-        //     "<strong>Please verify your account by <a href=\"https://api1.selfmade.ninja/verify?token=$token\">clicking here</a> or open this URL manually: <a href=\"https://api1.selfmade.ninja/verify?token=$token\">https://api1.selfmade.ninja/verify?token=$token</a></strong>"
-        // );
-        // $sendgrid = new \SendGrid($config['email_api_key']);
-        // try {
-        //     $response = $sendgrid->send($email);
-        //     // print $response->statusCode() . "\n";
-        //     // print_r($response->headers());
-        //     // print $response->body() . "\n";
-        // } catch (Exception $e) {
-        //     echo 'Caught exception: '. $e->getMessage() ."\n";
-        // }
-    }
-
-    public function getInsertID()
-    {
-        return $this->id;
-    }
-
-    public function userExists()
-    {
-        //TODO: Write the code to check if user exists.
-        return false;
-    }
-
-    public function hashPassword($cost = 10)
-    {
-        //echo $this->password;
-        $options = [
-            "cost" => $cost
-        ];
-        return password_hash($this->password, PASSWORD_BCRYPT, $options);
-    }
-
-    public static function verifyAccount($token)
-    {
-        $query = "SELECT * FROM apis.auth WHERE token='$token';";
-        $db = Database::getConnection();
-        $result = mysqli_query($db, $query);
-        if ($result and mysqli_num_rows($result) == 1) {
-            $data = mysqli_fetch_assoc($result);
-            if ($data['active'] == 1) {
-                throw new Exception("Already Verified");
+        $query="INSERT INTO `auth` (`username`, `password`, `email`, `active`, `token`, `signup_time`)
+        VALUES ('$username', '$pass', '$email', '0','$token', now())";
+       
+            if($this->conn->query($query)){
+               $this->id=$this->conn->insert_id;
             }
-            mysqli_query($db, "UPDATE `auth` SET `active` = '1' WHERE (`token` = '$token');");
+            else{
+                throw new Exception("error detected");
+            }
+    }
+    }
+    public function hashpass($cost=10){
+        $options=[
+            "cost" =>$cost
+        ];
+        $hash=password_hash($this->password, PASSWORD_BCRYPT,$options);
+        return $hash;
+    }
+
+    public function verificationmail(){
+        $file_details=file_get_contents("../../env.json");//gets the configuration details
+        $config=json_decode($file_details,true);
+        $mail=$config['email_username'];
+        $pass=$config['email_pass'];
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+
+
+//Load Composer's autoloader
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+   
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   ='nowsaymyname07@gmail.com';                     //SMTP username
+    $mail->Password   = 'ebeqhtywdwcistqb';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('nowsaymyname07@gmail.com', 'Mailer');
+    // $mail->addAddress('sachintheking25@gmail.com', 'Joe User');     //Add a recipient
+    $mail->addAddress($this->email);               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'test email';//need to add the verification link for the user profiles
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if($mail->send()){
+    return 'Message has been sent';
+    }
+    else{
+        return $mail->ErrorInfo;
+    }
+}
+
+    public function getinsertid(){
+        return $this->id;
+
+    }
+
+    public function userexists($name){
+        $sql="SELECT * FROM `auth` WHERE `username` = '$name'";
+        $res=$this->conn->query($sql);
+        if($res->num_rows==1){
             return true;
-        } else {
+        }
+        else{
             return false;
         }
     }
 }
+
